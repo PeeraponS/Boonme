0a2aa377dd8dffb27d0dd25ed1a7589168155bc8
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-04-06 17:31:21 +0700

    integrated mLab mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.argv[2] === "local"
     ? "mongodb://127.0.0.1:27017/boonme"
-    : "mongodb://heroku_ncn2fshw:tvufj6ra4eqf5bp1oaboo5cf5m@ds043057.mlab.com:43057/heroku_ncn2fshw",
+    : "mongodb://heroku_dz1ql4wx:t1qrdo2in3g6o06k372lsrgf16@ds153657.mlab.com:53657/heroku_dz1ql4wx",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
 );
