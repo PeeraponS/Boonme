ba08dc2766cf8dd1133d7923279a219396302458
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-04-08 12:40:26 +0700

    Add project_type_attr to project-table

--- src/models/project.js
+++ src/models/project.js
@@ -11,6 +11,28 @@ const projectSchema = new mongoose.Schema(
       required: true,
       ref: "User",
     },
+    project_type: {
+      type: String,
+      required: true,
+      default: "ทุนการศึกษา",
+    },
+    description: {
+      type: String,
+      required: true,
+      default:
+        "เสกสรรค์โหลนลอจิสติกส์หมายปองสหัชญาณ แทงโก้ตื้บผิดพลาดโก๊ะแจ๊กเก็ต โอเลี้ยงก๋ากั่น เพทนาการกรอบรูปท็อปบู๊ทโอเคไบเบิล ตะหงิดครัวซองต์รอยัลตี้ เหมยเซาท์หน่อมแน้มมายาคติ แฟรี่มาร์จินฮองเฮาจังโก้ สปอต ดีพาร์ทเมนต์อิออน แรงผลักเบอร์เกอร์ฟอยล์ด็อกเตอร์อิมพีเรียล ดีพาร์ตเมนท์คอนเทนเนอร์ทัวริสต์ ศากยบุตร พาร์ทเนอร์แผดเผาเฮียอพาร์ทเมนท์เอ็นจีโอ นิรันดร์เยลลี่มวลชนเทคโน ไตรมาสด็อกเตอร์เคลม ภูมิทัศน์",
+    },
+    img: {
+      type: String,
+      required: true,
+      default:
+        "https://static.posttoday.com/media/content/2018/04/26/27EF9A802B11483297A4F053A128FB8E.jpg",
+    },
+    due_date: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },
     donation_amount: {
       type: Number,
       required: true,
@@ -21,26 +43,9 @@ const projectSchema = new mongoose.Schema(
       required: true,
       default: 30000,
     },
-    due_date: {
-      type: Date,
-      required: true,
-      default: Date.now(),
-    },
-    description: {
-      type: String,
-      required: true,
-      default:
-        "เสกสรรค์โหลนลอจิสติกส์หมายปองสหัชญาณ แทงโก้ตื้บผิดพลาดโก๊ะแจ๊กเก็ต โอเลี้ยงก๋ากั่น เพทนาการกรอบรูปท็อปบู๊ทโอเคไบเบิล ตะหงิดครัวซองต์รอยัลตี้ เหมยเซาท์หน่อมแน้มมายาคติ แฟรี่มาร์จินฮองเฮาจังโก้ สปอต ดีพาร์ทเมนต์อิออน แรงผลักเบอร์เกอร์ฟอยล์ด็อกเตอร์อิมพีเรียล ดีพาร์ตเมนท์คอนเทนเนอร์ทัวริสต์ ศากยบุตร พาร์ทเนอร์แผดเผาเฮียอพาร์ทเมนท์เอ็นจีโอ นิรันดร์เยลลี่มวลชนเทคโน ไตรมาสด็อกเตอร์เคลม ภูมิทัศน์",
-    },
     is_completed: {
       type: Boolean,
       default: false,
-    },
-    img: {
-      type: String,
-      required: true,
-      default:
-        "https://static.posttoday.com/media/content/2018/04/26/27EF9A802B11483297A4F053A128FB8E.jpg",
     },
     address_blockchain: {
       type: String,
