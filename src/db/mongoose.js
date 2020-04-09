f17e2972c0b3edf8b0df28597541de16d6dc6ab2
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-04-09 14:22:24 +0700

    Setting environment variable up to heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.argv[2] === "local"
     ? process.env.LOCAL_MONGODB_URL
-    : process.env.HEROKU_MLAB_MONGODB_URL,
+    : process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
--- src/models/user.js
+++ src/models/user.js
@@ -118,7 +118,7 @@ userSchema.methods.generateAuthToken = async function () {
   const user = this;
 
   // get token
-  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
   user.tokens = user.tokens.concat({
     token,
   });
