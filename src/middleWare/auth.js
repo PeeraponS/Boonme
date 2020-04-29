66660cfdb8ab71ace7e959c206b624ddb6cacb80
Narawit Tubtimtoe <elesrubsoap_4822@hotmail.com>
2020-04-30 02:36:02 +0700

    DEBUG: auth problem when updating user-data

--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -4,11 +4,17 @@ const User = require("../models/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
+    console.log("token");
+    console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("decoded");
+    console.log(decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
     });
+    console.log("user");
+    console.log(user);
 
     if (!user) throw new Error();
 
